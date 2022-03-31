39576534035abe53255ac4f53a8f9c815046abcb
Gabriel Soares Gonçalves <gabrielsoaresg20@gmail.com>
2022-03-31 16:19:13 -0300

    Initial back-end

---
+++ src/main/java/br/sp/fatec/api/dw/DwApplication.java
@@ -0,0 +1,13 @@
+package br.sp.fatec.api.dw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/sp/fatec/api/dw/DwApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sp.fatec.api.dw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
