7aac4dd0b092f57140e216f110321231aa3b82d9
Gabriel Henrique de Souza Ferraz <bi.henriquesf@hotmail.com>
2022-04-01 21:39:11 -0300

    Comentando classes

--- src/main/java/br/sp/fatec/api/dw/model/CargoModel.java
+++ src/main/java/br/sp/fatec/api/dw/model/CargoModel.java
@@ -1,20 +1,19 @@
 package br.sp.fatec.api.dw.model;
 
-import lombok.Data;
-
-import javax.persistence.*;
-/*MANY TO ONE USUARIO*/
-
-@Data
-@Entity
-@Table(name = "Cargo")
-public class CargoModel {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "car_id ")
-    private Integer idCargo;
-
-    @Column(name = "car_descricao",nullable = false,unique = true,length = 50)
-    private String descricao;
-}
+//import lombok.Data;
+//
+//import javax.persistence.*;
+//
+//@Data
+//@Entity
+//@Table(name = "Cargo")
+//public class CargoModel {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "car_id ")
+//    private Integer idCargo;
+//
+//    @Column(name = "car_descricao",nullable = false,unique = true,length = 50)
+//    private String descricao;
+//}
