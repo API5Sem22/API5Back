5fd46bb3853070cb1667562f51d721a04c36f925
moonees <ramonzxz@icloud.com>
2022-04-07 10:59:44 -0300

    make better

--- src/main/java/br/sp/fatec/api/dw/usuarios/controlador/converter/HellouWordController.java
+++ src/main/java/br/sp/fatec/api/dw/usuarios/controlador/converter/HellouWordController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/hellou")
+@RequestMapping(value = "/hello")
 public class HellouWordController {
 
     @GetMapping
