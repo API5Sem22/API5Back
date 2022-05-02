3196a2b436ae107b3ca69cc763d59a26cd1b9e14
Gabriel Soares Gonçalves <gabrielsoaresg20@gmail.com>
2022-05-02 19:32:47 -0300

    err findbyId Cidade

--- src/main/java/br/sp/fatec/api/dw/empresa/repositorio/CidadeRepositorio.java
+++ src/main/java/br/sp/fatec/api/dw/empresa/repositorio/CidadeRepositorio.java
@@ -4,8 +4,10 @@ import br.sp.fatec.api.dw.empresa.modelo.CidadeModelo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CidadeRepositorio extends JpaRepository <CidadeModelo,Integer> {
 
-    CidadeModelo findByIdCidade(Integer id);
+    Optional<CidadeModelo> findById(Integer id);
 }
