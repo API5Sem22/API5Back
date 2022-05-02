495b373396fc0522fb3b4e6b6b5f90053f97aef4
Gabriel Soares Gonçalves <gabrielsoaresg20@gmail.com>
2022-05-02 19:30:10 -0300

    err findbyId Cnae

--- src/main/java/br/sp/fatec/api/dw/empresa/repositorio/CnaeRepositorio.java
+++ src/main/java/br/sp/fatec/api/dw/empresa/repositorio/CnaeRepositorio.java
@@ -4,8 +4,10 @@ import br.sp.fatec.api.dw.empresa.modelo.CnaeModelo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CnaeRepositorio extends JpaRepository <CnaeModelo,Integer> {
 
-    CnaeModelo findByIdCnae(Integer id);
+    Optional<CnaeModelo> findById(Integer id);
 }
