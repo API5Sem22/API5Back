c286abaec4ee600418f3aa3ffb117500c5a3dcf9
Gabriel Soares <61523979+GabrielSG20@users.noreply.github.com>
2022-05-15 22:20:26 -0300

    CrossOrigin EmpresaDescController (#25)

--- src/main/java/br/sp/fatec/api/dw/empresa/controlador/EmpresaDescricaoControlador.java
+++ src/main/java/br/sp/fatec/api/dw/empresa/controlador/EmpresaDescricaoControlador.java
@@ -5,12 +5,10 @@ import br.sp.fatec.api.dw.empresa.servico.EmpresaDescricaoServico;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/empresa-descricao")
 public class EmpresaDescricaoControlador {
 
